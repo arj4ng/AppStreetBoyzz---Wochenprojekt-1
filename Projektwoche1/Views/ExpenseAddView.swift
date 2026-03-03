33863685c848e65a6d4a372573e1ed1bcd7cba42
Hendrik <jordan_puls@yahoo.de>
2026-03-03 10:08:52 +0100

    ExpenseAddView hinzugefügt...

---
+++ Projektwoche1/Views/ExpenseAddView.swift
@@ -0,0 +1,18 @@
+//
+//  ExpenseAddView.swift
+//  Projektwoche1
+//
+//  Created by Hendrik Puls on 03.03.26.
+//
+
+import SwiftUI
+
+struct ExpenseAddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpenseAddView()
+}
