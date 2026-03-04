b4a84e3e1952cd9f1ae524d9b545a8c3bd0b8ec4
Hendrik <jordan_puls@yahoo.de>
2026-03-04 12:03:01 +0100

    Designanpasungen

--- Projektwoche1/Views/SubViews/AddBudgetView.swift
+++ Projektwoche1/Views/SubViews/AddBudgetView.swift
@@ -8,33 +8,47 @@ import SwiftData
 import SwiftUI
 
 struct AddBudgetView: View {
-    
+
     @Environment(\.modelContext) private var context
     @Environment(\.dismiss) private var dismiss
-    
+
     @State private var name: String = ""
     @State private var ammount: Double = 0.0
-    
+
     var body: some View {
         NavigationStack {
             Form {
-                TextField("Gib einen Budgetnamen ein ", text: $name)
-                Text("Dein Budget")
-                
-                HStack {
-                    Slider(value: $ammount, in: 0...1000, step: 10)
-                    Text("\(Int(ammount)) €")
-                        .bold()
-                }
-                
-                Button("Speichern") {
+                VStack {
+                    TextField("Gib einen Budgetnamen ein ", text: $name)
+                    Divider()
+                        .padding()
+                        .underline()
                     
-                    context.insert(Budget(
-                        name: name,
-                        plannedAmount: ammount)
+                    VStack {
+                        HStack {
+                            TextField(
+                                "Betrag",
+                                value: $ammount,
+                                format: .number
+                            )
+                            .keyboardType(.decimalPad)
+                            .padding(8)
+                            Image(systemName: "eurosign")
+
+                        }
+                        Slider(value: $ammount, in: 0...3000, step: 50)
+                    }
+                }
+                Button("Speichern") {
+
+                    context.insert(
+                        Budget(
+                            name: name,
+                            plannedAmount: ammount
+                        )
                     )
                     dismiss()
-                    
+
                     name = ""
                     ammount = 0.0
                 }
